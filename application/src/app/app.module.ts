f583f76b9db182beffca091809f44827a8817873
Fedor Usakov <FiodorUsakov@gmail.com>
2018-10-12 01:34:57 +0300

    Changed routes and imports

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { SharedModule } from '@app/shared';
+import { AppComponent } from '@app/app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
