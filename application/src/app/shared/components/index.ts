dcea1e097ea079da87c2f62958d975907100aa3c
Fedor Usakov <FiodorUsakov@gmail.com>
2018-11-13 23:01:26 +0300

    Started unified tile layout

--- application/src/app/shared/components/index.ts
+++ application/src/app/shared/components/index.ts
@@ -3,3 +3,4 @@ export * from '@app/shared/components/notification/notification.component';
 export * from '@app/shared/components/sidebar/sidebar.component';
 export * from '@app/shared/components/sidebar-lang-switcher/sidebar-lang-switcher.component';
 export * from '@app/shared/components/sidebar-navigation/sidebar-navigation.component';
+export * from '@app/shared/components/tile/tile.component';
