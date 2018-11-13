d7f3eb3b453a985d245ae54409ccf70f175c325d
Fedor Usakov <FiodorUsakov@gmail.com>
2018-11-13 22:18:25 +0300

    Simplified sidebar

--- application/src/app/shared/components/index.ts
+++ application/src/app/shared/components/index.ts
@@ -1,2 +1,5 @@
+export * from '@app/shared/components/logo/logo.component';
 export * from '@app/shared/components/notification/notification.component';
 export * from '@app/shared/components/sidebar/sidebar.component';
+export * from '@app/shared/components/sidebar-lang-switcher/sidebar-lang-switcher.component';
+export * from '@app/shared/components/sidebar-navigation/sidebar-navigation.component';
---
+++ application/src/app/shared/components/sidebar-navigation/sidebar-navigation.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavLinksConst } from '@app/constants';
+import { NavLinkModel } from '@app/models';
+
+@Component({
+  selector: 'app-sidebar-navigation',
+  templateUrl: './sidebar-navigation.component.html',
+  styleUrls: ['./sidebar-navigation.component.scss']
+})
+export class SidebarNavigationComponent {
+
+  public navLinks: NavLinkModel[] = NavLinksConst;
+
+}
