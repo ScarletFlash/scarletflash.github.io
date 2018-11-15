9c8f7efa1631599dd957720ac3ffc3e8bafea015
Fedor Usakov <FiodorUsakov@gmail.com>
2018-11-16 00:34:27 +0300

    Removed <div>s

--- application/src/app/pages/repositories/repository-tile/repository-tile.component.ts
+++ application/src/app/pages/repositories/repository-tile/repository-tile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { GithubRepositoryModel } from '@app/models';
 
 @Component({
   selector: 'app-repository-tile',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repository-tile.component.scss']
 })
 export class RepositoryTileComponent implements OnInit {
+
+  @Input() public repository: GithubRepositoryModel;
 
   constructor() { }
 
