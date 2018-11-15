ce0b40add666e08a4b78ddf8c2884fed1e46a443
Fedor Usakov <fiodorusakov@gmail.com>
2018-11-15 22:57:51 +0300

    Continued layout for pages

--- application/src/app/pages/repositories/repositories.module.ts
+++ application/src/app/pages/repositories/repositories.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { RepositoriesRoutingModule } from '@app/pages/repositories/repositories-routing.module';
 import { RepositoriesComponent } from '@app/pages/repositories/repositories.component';
 import { SharedModule } from '@app/shared';
+import { RepositoryTileComponent } from './repository-tile/repository-tile.component';
 
 @NgModule({
-  declarations: [RepositoriesComponent],
+  declarations: [RepositoriesComponent, RepositoryTileComponent],
   imports: [
     SharedModule,
     RepositoriesRoutingModule,
