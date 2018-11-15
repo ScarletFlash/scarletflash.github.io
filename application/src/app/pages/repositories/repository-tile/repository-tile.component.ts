712193eebed17011cad439bc5f03611949fc1adc
Fedor Usakov <fiodorusakov@gmail.com>
2018-11-15 22:57:51 +0300

    Continued layout for pages

---
+++ application/src/app/pages/repositories/repository-tile/repository-tile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repository-tile',
+  templateUrl: './repository-tile.component.html',
+  styleUrls: ['./repository-tile.component.scss']
+})
+export class RepositoryTileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- application/src/app/shared/components/search-input/search-input.component.ts
+++ application/src/app/shared/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-input',
@@ -7,12 +7,28 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class SearchInputComponent {
 
+  @Input() public placeholder?: string = '';
+
   @Output() public onvaluechange: EventEmitter<CustomEvent<string>> = new EventEmitter<CustomEvent<string>>(true);
+
+  private _focused: boolean = false;
 
   emitValue(value: string): void {
     this.onvaluechange.emit(
       new CustomEvent<string>('valuewaschanged', { detail: value })
     );
+  }
+
+  getElementStyles(element: HTMLElement): CSSStyleDeclaration {
+    return getComputedStyle(element);
+  }
+
+  setFocused(focused: boolean): void {
+    this._focused = focused;
+  }
+
+  getFocused(): boolean {
+    return this._focused;
   }
 
 }
