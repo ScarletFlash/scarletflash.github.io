f7e344a3be65e98d89f2f8cbe6c7176187a017fb
Fedor Usakov <fiodorusakov@gmail.com>
2018-11-23 00:06:58 +0300

    Added page for design system

---
+++ application/src/app/pages/design-system/design-system.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { DesignSystemRoutingModule } from '@app/pages/design-system/design-system-routing.module';
+import { DesignSystemComponent } from '@app/pages/design-system/design-system.component';
+import { SharedModule } from '@app/shared';
+
+@NgModule({
+  declarations: [DesignSystemComponent],
+  imports: [
+    CommonModule,
+    SharedModule,
+    DesignSystemRoutingModule
+  ]
+})
+export class DesignSystemModule { }
