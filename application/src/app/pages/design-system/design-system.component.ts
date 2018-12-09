353a3c72de4959b0acfd092b55b0f5cc2e5b609e
Fedor Usakov <fiodorusakov@gmail.com>
2018-12-09 17:57:21 +0300

    Fixed linter exceptions

--- application/src/app/pages/design-system/design-system.component.ts
+++ application/src/app/pages/design-system/design-system.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-design-system',
   templateUrl: './design-system.component.html',
   styleUrls: ['./design-system.component.scss']
 })
-export class DesignSystemComponent implements OnInit {
-  constructor() {}
-
-  public ngOnInit(): void {}
-}
+export class DesignSystemComponent {}
