18461c519aff3ce7344c794fda9fe1bfe9474818
Fedor Usakov <FiodorUsakov@gmail.com>
2018-12-09 18:06:10 +0300

    Fixed warnings

--- application/src/app/services/index.ts
+++ application/src/app/services/index.ts
@@ -1,3 +1,4 @@
 export * from '@app/services/requests';
+export * from '@app/services/github-controller.service';
 export * from '@app/services/notification.service';
 export * from '@app/services/repositories-page-state.service';
