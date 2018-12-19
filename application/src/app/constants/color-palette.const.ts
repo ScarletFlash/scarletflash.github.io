985b9505a7296e39aef23c602410edcfc8fae03e
Fedor Usakov <fiodorusakov@gmail.com>
2018-12-19 16:09:32 +0300

    Updated design system page

--- application/src/app/constants/color-palette.const.ts
+++ application/src/app/constants/color-palette.const.ts
@@ -1,5 +1,7 @@
 import { ColorPaletteItemInterface } from '@app/declarations';
 
 export const ColorPaletteConst: ColorPaletteItemInterface[] = [
-  { colorCode: '#ff5050', colorName: 'main' }
+  { colorCode: '#ff5050', colorName: 'main' },
+  { colorCode: '#171717', colorName: 'main' },
+  { colorCode: '#f4f4f4', colorName: 'body' }
 ];
