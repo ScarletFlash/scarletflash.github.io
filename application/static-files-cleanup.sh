04d80da78d83dbe25195f5765218b7d5eebd263d
Fedor Usakov <fiodorusakov@gmail.com>
2018-12-21 12:24:50 +0300

    Added build config

--- application/static-files-cleanup.sh
+++ application/static-files-cleanup.sh
@@ -3,7 +3,7 @@ cd ./../
 shopt -s extglob
 if [ -f ./index.html ]
 then
-    rm !(.vscode|application|LICENSE|README.md)
+    rm !(.vscode|.circleci|application|LICENSE|README.md)
     echo "Root directory was cleaned up."
 else
     echo "Root directory is clean."
