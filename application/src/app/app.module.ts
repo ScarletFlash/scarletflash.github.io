509a631a3d245639ddd3fd80015535b8782b6093
ScarletFlash <ScarletFlash.dev@gmail.com>
2019-05-05 22:42:40 +0300

    add primary main-page files

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [SharedModule, AppRoutingModule],
   providers: []
 })
 export class AppModule {}
---
+++ application/src/app/pages/main-page/main-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  styleUrls: ['./main-page.component.scss'],
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent {}
