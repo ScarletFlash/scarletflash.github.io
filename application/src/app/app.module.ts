1ea3da19c86b0ca1a0eaf53b742be1d9c2d95c08
ScarletFlash <scarletflash.dev@gmail.com>
2020-04-12 21:35:00 +0300

    feature(appearance) background animation; font-size media queries

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserAnimationsModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
