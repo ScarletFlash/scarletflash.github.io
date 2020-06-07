ea4b5f4d424a529682f65e8d0a0c14e77e46e54c
ScarletFlash <scarletflash.dev@gmail.com>
2020-06-07 21:46:09 +0300

    build(static files)

--- application/src/utils/is-nill.util.ts
+++ application/src/utils/is-nill.util.ts
@@ -1,7 +1,4 @@
-export function isNill(input: undefined): input is undefined;
-export function isNill(input: null): input is null;
-export function isNill<T>(input: T | null): input is null;
-export function isNill<T>(input: T | undefined): input is undefined;
+export function isNill<T>(input: T | undefined | null): input is undefined | null;
 export function isNill<T>(input: NonNullable<T>): false;
 export function isNill(input: unknown): input is null | undefined {
   return input === null || input === undefined;
