6a74025dafb5b7ab8341e0ab8c916a4897f793f9
ScarletFlash <scarletflash.dev@gmail.com>
2022-10-05 20:49:28 +0300

    chore: remove Angular-based website version

--- application/src/environments/development.env.ts
+++
@@ -1,7 +0,0 @@
-import 'zone.js/dist/zone-error';
-
-import type { Environment } from '@src/declarations/interfaces/environment.interface';
-
-export const environment: Environment = {
-  production: false
-};
--- application/src/utils/rxjs-operators/filter-by-instance-of.operator.ts
+++
@@ -1,9 +0,0 @@
-import { filter } from 'rxjs/operators';
-
-import type { Observable, OperatorFunction } from 'rxjs';
-
-export function filterByInstanceOf<T, K extends T>(target: any): OperatorFunction<T, K> {
-  return (source: Observable<T>): Observable<K> => {
-    return source.pipe(filter((data: T): data is K => data instanceof target));
-  };
-}
