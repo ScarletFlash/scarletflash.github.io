dbecd698ba7eee804c57cea89a98df61cd0b306f
ScarletFlash <scarletflash.dev@gmail.com>
2022-12-07 00:45:42 +0400

    build: initialize new NPM project

--- application/src/utils/is-nill.util.ts
+++
@@ -1,5 +0,0 @@
-export function isNill<T>(input: T | undefined | null): input is undefined | null;
-export function isNill<T>(input: NonNullable<T>): false;
-export function isNill(input: unknown): input is null | undefined {
-  return input === null || input === undefined;
-}
