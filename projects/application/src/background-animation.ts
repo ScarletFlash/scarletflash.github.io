c2303e0eca93ef22a9b3f8c7104d33350d6b37ce
ScarletFlash <scarletflash.dev@gmail.com>
2022-12-10 01:20:41 +0400

    build: add CI/CD

--- projects/application/src/background-animation.ts
+++ projects/application/src/background-animation.ts
@@ -1,14 +1,22 @@
 export class BackgroundAnimation {
   readonly #canvas: HTMLCanvasElement;
-  readonly #renderingContext: CanvasRenderingContext2D | null;
+  readonly #renderingContext: WebGL2RenderingContext | null;
 
   constructor(canvas: HTMLCanvasElement) {
     this.#canvas = canvas;
-    this.#renderingContext = this.#canvas.getContext('2d');
-    // this.#renderingContext = this.#canvas.getContext('webgl2', {
-    //   antialias: true,
-    //   failIfMajorPerformanceCaveat: true,
-    // });
+    this.#renderingContext = this.#canvas.getContext('webgl2', {
+      antialias: true,
+      failIfMajorPerformanceCaveat: true,
+    });
+  }
+
+  static #setupRenderingContext(context: WebGL2RenderingContext): void {
+    const { COLOR_BUFFER_BIT, DEPTH_BUFFER_BIT, DEPTH_TEST, LEQUAL }: WebGL2RenderingContext = context;
+    context.clearColor(1, 1, 1, 1);
+    context.enable(DEPTH_TEST);
+    context.depthFunc(LEQUAL);
+    // eslint-disable-next-line no-bitwise
+    context.clear(COLOR_BUFFER_BIT | DEPTH_BUFFER_BIT);
   }
 
   public run(): void {
@@ -16,13 +24,14 @@ export class BackgroundAnimation {
       throw new Error('WebGL is unavailable');
     }
 
+    BackgroundAnimation.#setupRenderingContext(this.#renderingContext);
     this.#applyShader();
     this.#makeCanvasVisible();
   }
 
   #applyShader(): void {
-    this.#renderingContext.fillStyle = '#ffffff';
-    this.#renderingContext.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
+    // this.#renderingContext.fillStyle = '#ffffff';
+    // this.#renderingContext.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
   }
 
   #makeCanvasVisible(): void {
