fb9a3f2411522dd41a9d5d174861d1f5ccbb5534
ScarletFlash <scarletflash.dev@gmail.com>
2022-12-15 02:57:01 +0400

    chore: add some animations to text content

--- projects/application/src/application.ts
+++ projects/application/src/application.ts
@@ -2,18 +2,29 @@ import { BackgroundAnimation } from './background-animation';
 
 export class Application {
   readonly #backgroundAnimation: BackgroundAnimation;
+  readonly #contentElement: HTMLElement;
 
   constructor() {
     const canvas: HTMLCanvasElement | null = document.querySelector('canvas.background-animation');
-
     if (canvas === null) {
       throw new Error('Canvas element is missing');
     }
-
     this.#backgroundAnimation = new BackgroundAnimation(canvas);
+
+    const contentElement: HTMLElement | null = document.querySelector('main.content');
+    if (contentElement === null) {
+      throw new Error('Contnet element is missing');
+    }
+    this.#contentElement = contentElement;
   }
 
   public start(): void {
     this.#backgroundAnimation.run();
+
+    this.#makeTextContentVisible();
+  }
+
+  #makeTextContentVisible(): void {
+    this.#contentElement.classList.remove('content_hidden');
   }
 }
