130ea27df54e6388e58839f303f9a9f59b9a1a4b
ScarletFlash <scarletflash.dev@gmail.com>
2022-12-18 21:16:55 +0400

    chore: maintain codebase

--- projects/scene/src/context.ts
+++ projects/scene/src/context.ts
@@ -1,11 +1,17 @@
 import type { WebGlView } from './declarations/web-gl-view.interface';
 
+interface Size {
+  widthPx: number;
+  heightPx: number;
+}
+
 export class Context implements WebGlView<WebGL2RenderingContext> {
   readonly #webGlRenderingContext: WebGL2RenderingContext;
 
   constructor(canvas: HTMLCanvasElement) {
     const context: WebGL2RenderingContext | null = canvas.getContext('webgl2', {
       antialias: true,
+      desynchronized: true,
       failIfMajorPerformanceCaveat: true,
     });
 
@@ -13,17 +19,25 @@ export class Context implements WebGlView<WebGL2RenderingContext> {
       throw new Error('WebGL is unaccessible');
     }
 
-    context.viewport(0, 0, context.canvas.width, context.canvas.height);
-    const { COLOR_BUFFER_BIT, DEPTH_TEST, LEQUAL }: WebGL2RenderingContext = context;
-    context.clearColor(1, 1, 1, 1);
-    context.enable(DEPTH_TEST);
-    context.depthFunc(LEQUAL);
-    context.clear(COLOR_BUFFER_BIT);
-
     this.#webGlRenderingContext = context;
+
+    const { COLOR_BUFFER_BIT, DEPTH_TEST, LEQUAL }: WebGL2RenderingContext = this.#webGlRenderingContext;
+    this.#webGlRenderingContext.clearColor(1, 1, 1, 1);
+    this.#webGlRenderingContext.enable(DEPTH_TEST);
+    this.#webGlRenderingContext.depthFunc(LEQUAL);
+    this.#webGlRenderingContext.clear(COLOR_BUFFER_BIT);
+
+    this.updateSize({
+      widthPx: canvas.width,
+      heightPx: canvas.height,
+    });
   }
 
   public get view(): WebGL2RenderingContext {
     return this.#webGlRenderingContext;
+  }
+
+  public updateSize({ widthPx, heightPx }: Size): void {
+    this.#webGlRenderingContext.viewport(0, 0, widthPx, heightPx);
   }
 }
--- projects/scene/src/declarations/shader-program-input.ts
+++ projects/scene/src/declarations/shader-program-input.ts
@@ -1,8 +1,7 @@
-import type { shaderProgram } from '../shader-program';
 import type { WebGlView } from './web-gl-view.interface';
 
 export interface ShaderProgramInput {
   context: WebGL2RenderingContext;
-  fragmentShader: WebGlView<shaderProgram>;
-  vertexShader: WebGlView<shaderProgram>;
+  fragmentShader: WebGlView<WebGLShader>;
+  vertexShader: WebGlView<WebGLShader>;
 }
