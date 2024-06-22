7004678726af1988b1034d925d2261f47cac43da
ScarletFlash <scarletflash.dev@gmail.com>
2024-06-22 16:11:13 +0200

    ci: formatting scripts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: ['./src/**/*.{html,ts}']
 };
