9caa163973591c04f143053c8994563b051289b3
ScarletFlash <scarletflash.dev@gmail.com>
2024-06-22 15:19:17 +0200

    chore: cleanup repo

--- projects/scene/src/fragment-shader.ts
+++
@@ -1,8 +0,0 @@
-import type { WebGlView } from './declarations/web-gl-view.interface';
-import { Shader } from './shader';
-
-export class FragmentShader extends Shader implements WebGlView<WebGLShader> {
-  constructor(context: WebGL2RenderingContext, sourceCode: string) {
-    super(context, sourceCode, context.FRAGMENT_SHADER);
-  }
-}
