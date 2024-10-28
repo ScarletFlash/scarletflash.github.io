ffcc12e2f18f514d9e28e6a92648e58cbeb653a8
ScarletFlash <scarletflash.dev@gmail.com>
2024-10-28 22:40:52 +0100

    chore: use outline for better accessability

--- src/app/pages/home/components/text-roulette-animated-item/text-roulette-animated-item.component.ts
+++ src/app/pages/home/components/text-roulette-animated-item/text-roulette-animated-item.component.ts
@@ -24,30 +24,35 @@ import {
         style({
           position: "absolute",
           left: "0",
+          transformOrigin: "right",
         })
       ),
       transition(":enter", [
         style({
           opacity: 0,
           position: "absolute",
-          transform: "translateY(180%)",
+          transform: "translate(10%, 180%) rotate(-1deg)",
         }),
         animate(
           "500ms ease-in-out",
           style({
             opacity: 1,
             position: "absolute",
-            transform: "translateY(0)",
+            transform: "translate(0, 0) rotate(0deg)",
           })
         ),
       ]),
       transition(":leave", [
-        style({ opacity: 1, position: "absolute", transform: "translateY(0)" }),
+        style({
+          opacity: 1,
+          position: "absolute",
+          transform: "translate(0, 0) rotate(0deg)",
+        }),
         animate(
           "500ms ease-in-out",
           style({
             opacity: 0,
-            transform: "translateY(-180%)",
+            transform: "translate(10%, -180%) rotate(1deg)",
             position: "absolute",
           })
         ),
