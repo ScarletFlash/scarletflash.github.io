089e0846326371ea07864b1ddb04d2a3e15df69c
ScarletFlash <scarletflash.dev@gmail.com>
2024-10-31 20:19:12 +0100

    feat: avatar placeholder

--- src/app/pages/home/components/avatar/avatar.component.ts
+++ src/app/pages/home/components/avatar/avatar.component.ts
@@ -1,4 +1,8 @@
-import { NgOptimizedImage } from "@angular/common";
+import {
+  IMAGE_LOADER,
+  ImageLoaderConfig,
+  NgOptimizedImage,
+} from "@angular/common";
 import {
   ChangeDetectionStrategy,
   Component,
@@ -9,6 +13,38 @@ import {
   selector: "app-avatar",
   standalone: true,
   imports: [NgOptimizedImage],
+  viewProviders: [
+    {
+      provide: IMAGE_LOADER,
+      useValue: ({
+        width,
+        src,
+        isPlaceholder: rawIsPlaceholder,
+      }: ImageLoaderConfig) => {
+        const [fileEntry, ...basePathParts]: string[] = src
+          .split("/")
+          .reverse();
+        const basePath: string = basePathParts.reverse().join("/");
+
+        const [fileExtension, ...fileNameParts]: string[] = fileEntry
+          .split(".")
+          .reverse();
+        const fileName: string = fileNameParts.reverse().join(".");
+
+        const isPlaceholder: boolean =
+          Boolean(rawIsPlaceholder) || typeof width !== "number";
+
+        const variableSuffix: string = Boolean(isPlaceholder)
+          ? ".placeholder"
+          : `-${width}w`;
+
+        return `/${basePath}/${fileName}${variableSuffix}.${fileExtension}`.replaceAll(
+          "//",
+          "/"
+        );
+      },
+    },
+  ],
   templateUrl: "./avatar.component.html",
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
