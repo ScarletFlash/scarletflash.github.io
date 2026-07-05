2a80e5d3d0e378f401707e15cf131f2519de78f9
ScarletFlash <scarletflash.dev@gmail.com>
2026-07-05 22:10:56 +0100

    chore: syncronize transitive Angular dependencies with the codebase

--- src/app/components/background/background.material.ts
+++ src/app/components/background/background.material.ts
@@ -1,6 +1,6 @@
 import { Color, ShaderMaterial, Uniform, Vector2 } from "three";
 import { RectSize } from "../../../declarations/types/rect-size.type";
-import { Timer } from "three/examples/jsm/Addons.js";
+import { Timer } from "three/examples/jsm/misc/Timer.js";
 
 const enum UniformName {
   CanvasSize = "canvasSize",
