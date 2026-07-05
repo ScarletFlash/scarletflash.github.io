3b4a249b049369d9f726d965ccb9cbce84963085
ScarletFlash <scarletflash.dev@gmail.com>
2026-07-05 19:52:28 +0100

    chore: remove deprecated @angular/animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { provideClientHydration, withNoIncrementalHydration } from "@angular/platform-browser";
-import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
@@ -10,6 +9,5 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withNoIncrementalHydration()),
-    provideAnimationsAsync(),
   ],
 };
--- src/app/pages/home/components/text-roulette-animated-item/text-roulette-animated-item.component.ts
+++ src/app/pages/home/components/text-roulette-animated-item/text-roulette-animated-item.component.ts
@@ -1,11 +1,4 @@
 import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger,
-} from "@angular/animations";
-import {
   ChangeDetectionStrategy,
   Component,
   ViewEncapsulation,
@@ -17,49 +10,8 @@ import {
   templateUrl: "./text-roulette-animated-item.component.html",
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: [
-    trigger("pop", [
-      state(
-        "*",
-        style({
-          position: "absolute",
-          left: "0",
-        })
-      ),
-      transition(":enter", [
-        style({
-          opacity: 0,
-          position: "absolute",
-          transform: "translate(10%, 180%) rotate(-1deg)",
-        }),
-        animate(
-          "500ms ease-in-out",
-          style({
-            opacity: 1,
-            position: "absolute",
-            transform: "translate(0, 0) rotate(0deg)",
-          })
-        ),
-      ]),
-      transition(":leave", [
-        style({
-          opacity: 1,
-          position: "absolute",
-          transform: "translate(0, 0) rotate(0deg)",
-        }),
-        animate(
-          "500ms ease-in-out",
-          style({
-            opacity: 0,
-            transform: "translate(10%, -180%) rotate(1deg)",
-            position: "absolute",
-          })
-        ),
-      ]),
-    ]),
-  ],
   host: {
-    "[@pop]": "",
+    class: "absolute left-0",
   },
 })
 export class TextRouletteAnimatedItemComponent {}
