590c203a871f0c640d660f62d5f0858ea08adb81
ScarletFlash <scarletflash.dev@gmail.com>
2026-07-05 23:32:18 +0100

    build: setup PostHog analytics

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { ApplicationConfig, provideAppInitializer, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { provideClientHydration, withNoIncrementalHydration } from "@angular/platform-browser";
 import { routes } from "./app.routes";
+import { initializePosthogAnalytics } from "./initializers/posthog-analytics.initializer";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withNoIncrementalHydration()),
+    provideAppInitializer(initializePosthogAnalytics),
   ],
 };
